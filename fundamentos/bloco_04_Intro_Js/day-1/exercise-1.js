26c7ebdef8ca8f6908bcdf00d511aa0a013f84a6
Joao Espanha <jpbesr@gmail.com>
2022-04-07 03:33:38 -0300

    doing the firsts 10 exercises

---
+++ fundamentos/bloco_04_Intro_Js/day-1/exercise-1.js
@@ -0,0 +1,21 @@
+//Program to show basic math operations
+
+const a = 10;
+const b = 5;
+
+
+
+console.log(a + b);
+
+//subtraction operation
+
+console.log(a - b);
+
+//multiplication operation
+console.log(a * b);
+
+//division operation
+console.log(a / b);
+
+//mudule operation
+console.log(b % a);
---
+++ fundamentos/bloco_04_Intro_Js/day-1/exercise-9.js
@@ -0,0 +1,13 @@
+const a = 8;
+const b = 7;
+const c = 6;
+
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
+    console.log('There is an odd nunber in there');
+
+
+
+} else {
+    console.log('There is no odd number in there')
+
+}
