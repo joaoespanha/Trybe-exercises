bf385dcad1d06b31f3febf39175b96e79438421e
Joao Espanha <jpbesr@gmail.com>
2022-06-22 18:53:43 -0300

    ercercicios dia 10.2

---
+++ front_end/bloco-10-intro-react/dia-1/app2/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Content/>
+    </>
+  );
+}
+
+export default App;
--- front_end/bloco-10-intro-react/dia-1/my-app/src/App.js
+++ front_end/bloco-10-intro-react/dia-1/my-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const Task = (value) => {
+    return (
+      <li>{value}</li>
+    );
+  }
+  const arryOfTasks = ['limpar a casa', 'fazer os aexercicios da trybe', 'treinar']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {arryOfTasks.map(Task)}
+    
+    </>
   );
 }
 
