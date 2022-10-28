ae328926157203276b5135b97a85715b43511ddd
Joao Espanha <jpbesr@gmail.com>
2022-10-28 19:39:04 -0300

    adding GET and Post routes

---
+++ back_end/bloco-23-intro-node-express/movies_api/src/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+const dataBase = 'src/movies.json';
+
+app.use(express.json());
+
+// route to get movies arry
+app.get('/movies', async (req, res) => {
+    const data = await fs.readFile(dataBase, 'utf-8');
+
+    console.log(JSON.parse(data));
+    res.status(200).json(data);
+});
+
+// route to get movie by id
+app.get('/movies/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const data = await fs.readFile(dataBase, 'utf-8');
+    const moviesArry = JSON.parse(data);
+
+    const selectedMovie = moviesArry.filter((movie) => movie.id === Number(id));
+
+    console.log(selectedMovie);
+
+    res.status(200).json(selectedMovie);
+});
+
+// route to add a new movie
+
+app.post('/movies', async (req, res) => {
+    const newMovie = { ...req.body };
+
+    const oldData = await fs.readFile(dataBase, 'utf-8');
+    const moviesList = JSON.parse(oldData);
+
+    // adding to the dataBase json
+    moviesList.push(newMovie);
+    const listToString = JSON.stringify(moviesList);
+
+    await fs.writeFile(dataBase, listToString);
+    
+    // response
+    console.log(moviesList);
+    res.status(200).json(moviesList);
+});
+
+module.exports = app;
---
+++ back_end/bloco-23-intro-node-express/movies_api/src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+const SERVER_PORT = 3002;
+
+app.listen(SERVER_PORT, () => console.log(`server runing on port ${SERVER_PORT} `));
